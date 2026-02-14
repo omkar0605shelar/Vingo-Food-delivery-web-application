398a4084e4eb91667953e8ac3653aa1a39b5db3a
Omkar <shelaromkar313@gmail.com>
2026-02-15 00:19:59 +0530

    Corrected typos in the documentation.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import UserDashboard from "../components/UserDashboard";
-import OwnerDashboard from "../components/OwnerDashboard";
-import DeliveryBoy from "../components/DeliveryBoy";
+import UserDashboard from "../components/UserDashboard.jsx";
+import OwnerDashboard from "../components/OwnerDashboard.jsx";
+import DeliveryBoy from "../components/DeliveryBoy.jsx";
 
 function Home() {
   const { userData } = useSelector((state) => state.user);
