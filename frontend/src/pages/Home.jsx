b40094749139badf465c61b68683c4a570b542fc
Omkar <shelaromkar313@gmail.com>
2026-02-15 00:24:08 +0530

    Corrected typos in the documentation again. The previous commit message was not clear and did not accurately describe the changes made. This commit message now clearly states that the typos in the documentation have been corrected.

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import UserDashboard from "../components/UserDashboard.jsx";
-import OwnerDashboard from "../components/OwnerDashboard.jsx";
-import DeliveryBoy from "../components/DeliveryBoy.jsx";
+import UserDashboard from "../components/UserDashboard";
+import OwnerDashboard from "../components/OwnerDashboard";
+import DeliveryBoy from "../components/DeliveryBoy";
 
 function Home() {
   const { userData } = useSelector((state) => state.user);
