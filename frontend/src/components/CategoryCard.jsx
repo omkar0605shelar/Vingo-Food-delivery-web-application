0b8502e20d327c1c87ea3b7b74bb3c717146acd3
Omkar Shelar <shelaromkar313@gmail.com>
2026-05-26 23:28:43 +0530

    Fix JSX syntax in CategoryCard component

--- frontend/src/components/CategoryCard.jsx
+++ frontend/src/components/CategoryCard.jsx
@@ -6,6 +6,7 @@ function CategoryCard({name,image,onClick}) {
      <img src={image} alt="" className=' w-full h-full object-cover transform hover:scale-110 transition-transform duration-300'/>
      <div className='absolute  bottom-0 w-full left-0  bg-[#ffffff96] bg-opacity-95 px-3 py-1 rounded-t-xl text-center shadow text-sm font-medium text-gray-800 backdrop-blur'>
 {name}
+       
      </div>
     </div>
   )
